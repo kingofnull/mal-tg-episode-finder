4816c35166c80f0806404e7efad06894929e394f
Gitea <mssblackhole@gmail.com>
2022-05-12 22:55:05 +0430

    add auto reload on changing episod and increase watched episod

--- IntegratedScript.js
+++ IntegratedScript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Telegram new episode finder for MAL watching list page and anime list
 // @namespace    https://github.com/kingofnull/mal-tg-episode-finder
-// @version      2.00
+// @version      2.30
 // @description  Find new unwatched episode from your disired Telegram channel/group/peer... and show a link to it in front of each anime on your MAL CURRENTLY WATCHING page.
 // @author       KingOfNull
 // @homepage     https://github.com/kingofnull/mal-tg-episode-finder
@@ -42,7 +42,7 @@ async function initTgClient(){
     const client = new TelegramClient(new StringSession(session), apiId, apiHash, {
         connectionRetries: 3,
     });
-    
+
     //await client.connect();
     await client.start({
         phoneNumber: async () => prompt("Please enter your number: "),
@@ -54,12 +54,12 @@ async function initTgClient(){
     client.session.setDC(4, 'kws4.web.telegram.org', 443);//wss://kws4.web.telegram.org/apiws
 
     localStorage.setItem('TgStrSession',client.session.save());
-    
+
     return client;
 }
 
 function getPeerIds(){
-     //find your peerId using @username_to_id_bot in telegram
+    //find your peerId using @username_to_id_bot in telegram
     let peerId=localStorage.getItem("TgSearchPeerId");
     if(!peerId){
         peerId=prompt("Enter your search target peerId:");
@@ -67,41 +67,41 @@ function getPeerIds(){
     }
 
     let peerIds=peerId.split(",").map(p=>p*1);
-    
+
     return peerIds;
-    
+
 }
 
 async function findNewEp(client,name,ep,peerId,withOutLeadingZero){
-        const {Api} = gramjs;
-        let nextEpStr=(ep*1+1).toString();
+    const {Api} = gramjs;
+    let nextEpStr=(ep*1+1).toString();
+    if(!withOutLeadingZero){
+        nextEpStr=nextEpStr.padStart(2,'0');
+    }
+
+    let searchTerm=`"${name} ${nextEpStr}"`;
+    console.log("search for:",searchTerm);
+    const result = await client.invoke(
+        new Api.messages.Search({
+            peer: peerId,
+            q: searchTerm,
+            filter: new Api.InputMessagesFilterDocument({}),
+            limit:10,
+        })
+    );
+
+    if(result.count<1){
+        console.warn("not found");
         if(!withOutLeadingZero){
-            nextEpStr=nextEpStr.padStart(2,'0');
+            return await findNewEp(client,name,ep,peerId,true);
         }
+        return null;
+    }
 
-        let searchTerm=`"${name} ${nextEpStr}"`;
-        console.log("search for:",searchTerm);
-        const result = await client.invoke(
-            new Api.messages.Search({
-                peer: peerId,
-                q: searchTerm,
-                filter: new Api.InputMessagesFilterDocument({}),
-                limit:10,
-            })
-        );
-
-        if(result.count<1){
-            console.warn("not found");
-            if(!withOutLeadingZero){
-                return await findNewEp(client,name,ep,peerId,true);
-            }
-            return null;
-        }
-
-        let message=result.messages.at(0);
-        let fname=message.media.document.attributes.at().fileName;
-        console.log(result.count,fname); // prints the result
-        /*
+    let message=result.messages.at(0);
+    let fname=message.media.document.attributes.at().fileName;
+    console.log(result.count,fname); // prints the result
+    /*
         const linkResult = await client.invoke(
             new Api.channels.ExportMessageLink({
                 channel: peerId,
@@ -111,9 +111,9 @@ async function findNewEp(client,name,ep,peerId,withOutLeadingZero){
         );
         let link=linkResult.link;
 */
-        let link=`tg://privatepost?channel=${message.peerId.channelId}&post=${message.id}`;
-        console.log(link);
-        return {link,fname};
+    let link=`tg://privatepost?channel=${message.peerId.channelId}&post=${message.id}`;
+    console.log(link);
+    return {link,fname};
 }
 
 
@@ -155,16 +155,56 @@ async function searchAllPeers(client,title,curEp){
             break;
         }
     }
-    
+
     return data;
+}
+
+async function findAndShowLink($r,curEp,_client){
+    $r.siblings('.new-ep-link').remove();
+    let client;
+    if(!_client){
+        client=await initTgClient();
+    }else{
+        client=_client;
+    }
+
+    let title=initRenamer($r);
+    /*
+    let curEp=$("#myinfo_watchedeps").val();
+
+     */
+
+    if(!Number(curEp)){
+        curEp="0";
+    }
+    console.log(title,":",curEp);
+
+
+    let data=await searchAllPeers(client,title,curEp);
+
+    if(data){
+        $("<a>",{href:data.link,class:'new-ep-link',target:"tgLinksWin", id:'next-ep-link'})
+            .text("New Ep ! : "+data.fname)
+            .appendTo($r.parent());
+    }
+
+    if(!_client){
+        client.disconnect();
+    }
+}
+
+async function findAndShowLinkSinglePage(){
+    let $r=$(".title-name").first();
+    let ep=$("input#myinfo_watchedeps.inputtext.js-user-episode-seen").val()*1;
+    await findAndShowLink($r, ep);
 }
 
 (async ()=> {
     await addScript('https://cdn.jsdelivr.net/gh/kingofnull/gramjs-browser-build@69e9b471e30bb6da54577f911c953e1bf646b98b/gramjs.js');
-    
-    
+
+
     //-----------------------------------watching list page handler------------------------------------------//
-    
+
     if(/https:\/\/myanimelist.net\/animelist\/[^\/]/i.test(document.URL)){
         GM_addStyle(`
                 .new-ep-link{
@@ -198,37 +238,54 @@ async function searchAllPeers(client,title,curEp){
                     color:  #009688 !important;
                 }
 
+                input{
+                  color:black !important;
+
+                }
+
         `);
 
-        
-       let client=await initTgClient();
-       for(let r of $("td.title a.link").get()){
+
+        let client=await initTgClient();
+        for(let r of $("td.title a.link").get()){
             let $r=$(r);
             //let title=$r.text();
-            let title=initRenamer($r);
+            //let title=initRenamer($r);
             let curEp=$r.parents("tr").find("td.progress a.link").text();
+            await findAndShowLink($r,curEp,client);
+
+            $r.parents("tr").find(".icon-add-episode").on("click", e=> {
+                let _curEp=$r.parents("tr").find("td.progress a.link").text();
+                findAndShowLink($r,_curEp);
+            });
+
+             $r.parents("tr").on('change','.progress input', e=> {
+                let _curEp=$r.parents("tr").find("td.progress a.link").text();
+                findAndShowLink($r,_curEp);
+            });
+/*
             if(!Number(curEp)){
                 curEp="0";
             }
 
             title=initRenamer($r);
-            
+
             console.log(title,":",curEp);
-            
+
             let data=await searchAllPeers(client,title,curEp);
 
             if(data){
                 $("<a>",{href:data.link,class:'new-ep-link',target:"tgLinksWin"})
                     .text("New Ep ! : "+data.fname)
                     .appendTo($r.parents("td"));
-            }
-        } 
+            } */
+        }
         client.disconnect();
-    }    
-    
-    
+    }
+
+
     //-------------------------------- Anime page handler------------------------------------------//
-    
+
     if(/https:\/\/myanimelist.net\/anime\/\d+\/[^\/]/i.test(document.URL)){
         GM_addStyle(`
             .new-ep-link{
@@ -263,12 +320,12 @@ async function searchAllPeers(client,title,curEp){
                 font-family: monospace !important;
             }
         `);
-        
+        /*
         async function findAndShowLink(){
             $('#next-ep-link').remove();
             let client=await initTgClient();
-            
-          
+
+
 
             let $r=$(".title-name").first();
             let title=initRenamer($r);
@@ -281,24 +338,27 @@ async function searchAllPeers(client,title,curEp){
 
 
             let data=await searchAllPeers(client,title,curEp);
-            
+
             if(data){
                 $("<a>",{href:data.link,class:'new-ep-link',target:"tgLinksWin", id:'next-ep-link'})
-                        .text("New Ep ! : "+data.fname)
-                        .appendTo($r.parent());
+                    .text("New Ep ! : "+data.fname)
+                    .appendTo($r.parent());
             }
-            
+
             client.disconnect();
-            
+
         }
-        
-        $(".js-btn-count").on("click", ()=>findAndShowLink());
-        $("#myinfo_watchedeps").on('change blure',e=>fetchDlLink());
-        findAndShowLink();
+ */
+
+
+
+        $(".js-btn-count").on("click", e=> findAndShowLinkSinglePage());
+        $('input#myinfo_watchedeps.inputtext.js-user-episode-seen').on('change',e=> findAndShowLinkSinglePage() );
+        findAndShowLinkSinglePage();
     }
-    
 
-    
 
-    
+
+
+
 })();
