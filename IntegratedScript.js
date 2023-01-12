3063db9dba28f6ea557bb7cd22e103fb198be0ba
Gitea <mssblackhole@gmail.com>
2023-01-12 21:40:23 +0330

    optimizing search mechanism,adding progress bar,some style improving

--- IntegratedScript.js
+++ IntegratedScript.js
@@ -12,6 +12,39 @@
 // @license MIT
 
 // ==/UserScript==
+function beep(duration, freq,gain, finishedCallback) {
+    // I create the class with best available
+    var ctxClass = window.audioContext || window.AudioContext || window.AudioContext || window.webkitAudioContext
+    // We instance the class, create the context
+    var ctx = new ctxClass();
+    // Create the oscillator
+    var osc = ctx.createOscillator();
+    // Define type of wave
+    osc.type = 'sine';
+    // We create a gain intermediary
+    var volume = ctx.createGain();
+    // We connect the oscillator with the gain knob
+    osc.connect(volume);
+    // Then connect the volume to the context destination
+    volume.connect(ctx.destination);
+    // We can set & modify the gain knob
+    volume.gain.value = gain || 1;
+
+    //We can test it with some frequency at current time
+    osc.frequency.setValueAtTime(freq, ctx.currentTime);
+    if (osc.noteOn) osc.noteOn(0);
+    if (osc.start) osc.start();
+
+    // We'll have to stop it at some point
+    setTimeout(function () {
+        if (osc.noteOff) osc.noteOff(0);
+        if (osc.stop) osc.stop();
+        // We can insert a callback here, let them know you've finished, may be play next note?
+        finishedCallback && finishedCallback();
+    }, duration);
+
+};
+
 
 
 
@@ -73,6 +106,8 @@ function getPeerIds(){
 }
 
 async function findNewEp(client,name,ep,peerId,withOutLeadingZero){
+    console.log('search in ',peerId);
+
     const {Api} = gramjs;
     let nextEpStr=(ep*1+1).toString();
     if(!withOutLeadingZero){
@@ -81,14 +116,31 @@ async function findNewEp(client,name,ep,peerId,withOutLeadingZero){
 
     let searchTerm=`"${name} ${nextEpStr}"`;
     console.log("search for:",searchTerm);
-    const result = await client.invoke(
-        new Api.messages.Search({
-            peer: peerId,
-            q: searchTerm,
-            filter: new Api.InputMessagesFilterDocument({}),
-            limit:10,
-        })
-    );
+    let result =[];
+    if(peerId){
+        result = await client.invoke(
+            new Api.messages.Search({
+                peer: peerId,
+                q: searchTerm,
+                filter: new Api.InputMessagesFilterDocument({}),
+                limit:1,
+            })
+        );
+    }else{
+        result = await client.invoke(
+            new Api.messages.SearchGlobal({
+                q: searchTerm,
+                filter: new Api.InputMessagesFilterDocument({}),
+                // minDate: 43,
+                // maxDate: 43,
+                // offsetRate: 43,
+                offsetPeer:  new Api.InputPeerEmpty(),
+                // offsetId: 43,
+                limit: 100,
+            })
+        );
+    }
+   
 
     if(result.count<1){
         console.warn("not found");
@@ -98,10 +150,14 @@ async function findNewEp(client,name,ep,peerId,withOutLeadingZero){
         return null;
     }
 
-    let message=result.messages.at(0);
-    let fname=message.media.document.attributes.at().fileName;
-    console.log(result.count,fname); // prints the result
-    /*
+
+
+    let resultList=result.messages
+    .filter(m=>!!m.document)
+    .map(message=>{
+        let fname=message.document.attributes.at().fileName;
+        console.log(result.count,fname); // prints the result
+        /*
         const linkResult = await client.invoke(
             new Api.channels.ExportMessageLink({
                 channel: peerId,
@@ -109,11 +165,19 @@ async function findNewEp(client,name,ep,peerId,withOutLeadingZero){
                 thread: true,
             })
         );
-        let link=linkResult.link;
-*/
-    let link=`tg://privatepost?channel=${message.peerId.channelId}&post=${message.id}`;
-    console.log(link);
-    return {link,fname};
+        let link=linkResult.link;*/
+        let link=`tg://privatepost?channel=${message.peerId.channelId}&post=${message.id}`;
+        console.log(link);
+        return {link,fname,peerId:message.chatId};
+
+    });
+
+    if(peerId){
+        return resultList.at(0);
+    }
+
+
+    return resultList;
 }
 
 
@@ -144,10 +208,21 @@ function initRenamer($title){
 
 async function searchAllPeers(client,title,curEp,find_all){
     let dataList=[];
+    let foundList=await findNewEp(client,title,curEp,null);
+ for(let pId of getPeerIds()){
+     let data=foundList.find(r=>r.peerId==pId);
+            if(data){
+
+                dataList.push(data);
+            }
+
+ }
+/*
     for(let pId of getPeerIds()){
         try{
             let data=await findNewEp(client,title,curEp,pId);
             if(data){
+
                 dataList.push(data);
             }
         }catch(e){
@@ -157,48 +232,63 @@ async function searchAllPeers(client,title,curEp,find_all){
         if(!find_all && dataList.length>0){
             break;
         }
-    }
+    } */
 
     return dataList;
 }
 
 async function findAndShowLink($r,curEp,_client,find_all){
-    $r.siblings('.new-ep-link,.new-ep-link-br').remove();
     let client;
-    if(!_client){
-        client=await initTgClient();
-    }else{
-        client=_client;
+
+    try{
+
+        if(!_client){
+            client=await initTgClient();
+        }else{
+            client=_client;
+        }
+
+        $r.siblings('.new-ep-link,.new-ep-link-br').remove();
+        let title=initRenamer($r);
+        /*
+    let curEp=$("#myinfo_watchedeps").val();
+
+     */
+
+        if(!Number(curEp)){
+            curEp="0";
+        }
+        console.log(title,":",curEp);
+
+
+        let dataList=await searchAllPeers(client,title,curEp,find_all);
+
+        for(let data of dataList){
+            $("<a>",{href:data.link,class:'new-ep-link',target:"tgLinksWin", id:'next-ep-link'})
+                .text(data.fname)
+                .attr('title',data.fname)
+                .appendTo($r.parent());
+            $("<br>",{class:'new-ep-link-br'})
+                .appendTo($r.parent());
+        }
+
+
+
+    }catch(err){
+        console.error(err);
+    }finally{
+        if(!_client){
+            client.disconnect();
+        }
     }
 
-    let title=initRenamer($r);
-
-    if(!Number(curEp)){
-        curEp="0";
-    }
-    console.log(title,":",curEp);
-
-
-    let dataList=await searchAllPeers(client,title,curEp,find_all);
-
-    for(let data of dataList){
-        $("<a>",{href:data.link,class:'new-ep-link',target:"tgLinksWin", id:'next-ep-link'})
-            .text("New Ep ! : "+data.fname)
-            .appendTo($r.parent());
-        $("<br>",{class:'new-ep-link-br'})
-            .appendTo($r.parent());
-    }
-
-
-    if(!_client){
-        client.disconnect();
-    }
 }
 
 async function findAndShowLinkSinglePage(){
     let $r=$(".title-name").first();
     let ep=$("input#myinfo_watchedeps.inputtext.js-user-episode-seen").val()*1;
     await findAndShowLink($r, ep,null,true);
+    beep(150, 666,0.08);
 }
 
 (async ()=> {
@@ -216,7 +306,15 @@ async function findAndShowLinkSinglePage(){
                  border-radius: 7px;
                  display:inline-block;
                  padding:5px;
-                 margin-left: 0.5em;
+                 max-width: 100%;
+                 word-break: break-all;
+
+                 margin: 0 0 0.5em 0!important;
+                 white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                max-width: 21em;
+
                 }
 
                 .list-table .list-table-data .data a.new-ep-link:hover {
@@ -245,29 +343,64 @@ async function findAndShowLinkSinglePage(){
 
                 }
 
+
+
         `);
 
+        async function checkAllWatchingList(){
 
-        let client=await initTgClient();
-        for(let r of $("td.title a.link").get()){
-            let $r=$(r);
-            //let title=$r.text();
-            //let title=initRenamer($r);
-            let curEp=$r.parents("tr").find("td.progress a.link").text();
-            await findAndShowLink($r,curEp,client);
+            let client=await initTgClient();
+            try{
+                let $progressBar=$("<div class='list-status-title'>").append("<h2 align=center>TEF Searching . . .</h2>").insertAfter('.list-status-title');
+                let i=0;
+                let titleList=$("td.title a.link").get();
+                for(let r of titleList){
+                    let $r=$(r);
+                    //let title=$r.text();
+                    //let title=initRenamer($r);
+                    let curEp=$r.parents("tr").find("td.progress a.link").text();
+                    await findAndShowLink($r,curEp,client);
 
-            $r.parents("tr").find(".icon-add-episode").on("click", e=> {
-                let _curEp=$r.parents("tr").find("td.progress a.link").text();
-                findAndShowLink($r,_curEp);
-            });
+                    $r.parents("tr").find(".icon-add-episode").on("click", e=> {
+                        let _curEp=$r.parents("tr").find("td.progress a.link").text();
+                        findAndShowLink($r,_curEp);
+                    });
 
-             $r.parents("tr").on('change','.progress input', e=> {
-                let _curEp=$r.parents("tr").find("td.progress a.link").text();
-                findAndShowLink($r,_curEp);
-            });
+                    $r.parents("tr").on('change','.progress input', e=> {
+                        let _curEp=$r.parents("tr").find("td.progress a.link").text();
+                        findAndShowLink($r,_curEp);
+                    });
+                    /*
+            if(!Number(curEp)){
+                curEp="0";
+            }
+
+            title=initRenamer($r);
+
+            console.log(title,":",curEp);
+
+            let data=await searchAllPeers(client,title,curEp);
+
+            if(data){
+                $("<a>",{href:data.link,class:'new-ep-link',target:"tgLinksWin"})
+                    .text("New Ep ! : "+data.fname)
+                    .appendTo($r.parents("td"));
+            } */
+                    i++;
+                    $progressBar.css('background-image',`linear-gradient(90deg, #00a1a9 ${(i/titleList.length)*100}%, transparent 0)`);
+                }
+                document.querySelectorAll(".list-item").forEach(e=>e.style.order=(e.querySelector('.new-ep-link')?1:2))
+                $progressBar.remove();
+            }catch(err){
+                console.error(err);
+            }finally{
+                client.disconnect();
+            }
+
 
         }
-        client.disconnect();
+        checkAllWatchingList();
+        //setInterval(()=>checkAllWatchingList(),3600);
     }
 
 
@@ -309,12 +442,42 @@ async function findAndShowLinkSinglePage(){
                 font-family: monospace !important;
             }
         `);
+        /*
+        async function findAndShowLink(){
+            $('#next-ep-link').remove();
+            let client=await initTgClient();
+
+
+
+            let $r=$(".title-name").first();
+            let title=initRenamer($r);
+
+            let curEp=$("#myinfo_watchedeps").val();
+            if(!Number(curEp)){
+                curEp="0";
+            }
+            console.log(title,":",curEp);
+
+
+            let data=await searchAllPeers(client,title,curEp);
+
+            if(data){
+                $("<a>",{href:data.link,class:'new-ep-link',target:"tgLinksWin", id:'next-ep-link'})
+                    .text("New Ep ! : "+data.fname)
+                    .appendTo($r.parent());
+            }
+
+            client.disconnect();
+
+        }
+ */
 
 
 
         $(".js-btn-count").on("click", e=> findAndShowLinkSinglePage());
         $('input#myinfo_watchedeps.inputtext.js-user-episode-seen').on('change',e=> findAndShowLinkSinglePage() );
         findAndShowLinkSinglePage();
+        //setInterval(()=>findAndShowLinkSinglePage(),3600);
     }
 
 
